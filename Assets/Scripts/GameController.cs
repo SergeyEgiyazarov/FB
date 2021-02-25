8d9010a4c9bcdb4d9308fe781367ccf537d870c5
Sergey Egiyazarov <egiyazarov.s@gmail.com>
2021-02-25 20:11:43 +0300

    Clone Flappy Birds

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject gameOverText;
+    public Text scoreText;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
+    private static GameController instance;
+    private int score = 0;
+
+    public static GameController Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (gameOver == true && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
